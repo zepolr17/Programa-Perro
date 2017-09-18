eb6f5f0957d289ee8e4a8ec51a3d95a1a315191c
Vivi <Vivi@Viviana>
2017-09-18 12:24:53 -0500

    Pagina web con cuadros de texto y boton

---
+++ Pagina web.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+    <body>
+        <form action="procesador">
+            <p>id</p>
+           id <input type="text" name="id">
+            <br>
+            <p>raza</p>
+           Raza <input type="text" name="raza">
+            <br>
+            <p>nom</p>
+            Nombre <input type="text" name="no">
+            <br>
+            <p>edad</p>
+            Edad <input type="text" name="edad">
+            <br>
+            <p>kg</p>
+            Peso(kg)<input type="text" name="kg">
+            <br>
+            <input type="submit" value="enviar">
+            
+        </form>
+    </body>
+    </head>
+</html>
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
