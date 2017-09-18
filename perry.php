2cbc7290da6ef928a336f86cf240ee43db3c769f
Vivi <Vivi@Viviana>
2017-09-18 13:08:55 -0500

    Clase perro

---
+++ perry.php
@@ -0,0 +1,16 @@
+<?php
+class perry{
+
+var $id;
+    var $nombre;
+    var $edad;
+    function guardarperro(){
+        
+    }
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
